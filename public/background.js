a209f9fe851f6e10118bc3492b5f484aada9e7fe
Guo Yunhe <i@guoyunhe.me>
2026-08-17 15:37:23 +0800

    chore(vite): entry

--- public/background.js
+++
@@ -1,1 +0,0 @@
-// we don't need to do anything in background.js for now, but maybe in the future
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,19 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
   input: {
-    popup: resolve(import.meta.dirname, 'popup.html'),
+    background: resolve(import.meta.dirname, 'src/background/index.ts'),
+    content: resolve(import.meta.dirname, 'src/content/index.ts'),
     options: resolve(import.meta.dirname, 'options.html'),
+    popup: resolve(import.meta.dirname, 'popup.html'),
   },
   plugins: [react()],
 });
